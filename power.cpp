d111d70a5feef608719c777b9c8c152fbfd9e6c1
rINAtvERESHAGIN <vereshaginrin@gmail.com>
2018-07-11 14:19:51 +0300

    we learn doxygen , this is very nice

--- power.cpp
+++ power.cpp
@@ -1,13 +1,32 @@
+/*! \file */ 
 #include <iostream>
-	int main(){
-int nb=2;
-	for(int ni=0;ni<10;++ni){
-int np;
-np=1;
-	for(int nt=1;nt<=i;++nt){
-	np=np*nb;
-	std::cout<<np<<" ";
-	}
-std::cout<<std::endl;
-	return 0;
+#include <string>
+using namespace std;
+
+void printMyMind(string s){
+cout<<s;
 }
+//! How old are you method
+/*! \param age Vozrast
+*/
+int howOldAreU(int age){
+return age;
+}
+//!Главый метод 
+	int main(){
+//!Сделан Рефрактонин по Венрегскому методу , верблюд первый малый 
+int nb=2;
+//!Первый цикл 
+	for(int ni=0;ni<10;++ni){
+int np = 1;
+//!Второй цикл 
+	for(int nt=1;nt<=i;++nt)
+	np=np*nb;
+//!Вывод на экран.!!!
+	cout<<np<<" ";
+	}
+//!Practica VERY GOOD!
+cout<<endl;
+	return 0;
+  }
+
