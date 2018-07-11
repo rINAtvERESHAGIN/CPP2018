2034f18175bbb9837646c6eb8f1ce4dba62876b0
rINAtvERESHAGIN <vereshaginrin@gmail.com>
2018-07-11 11:25:43 +0300

    Refracting

--- power.cpp
+++ power.cpp
@@ -2,7 +2,19 @@
 int main()
 {
 int b=2;
-for(int i=0;i<10;++i)
+#include <iostream>
+	int main(){
+int nb=2;
+	for(int ni=0;ni<10;++ni){
+int np;
+np=1;
+	for(int nt=1;nt<=i;++nt){
+	np=np*nb;
+	std::cout<<np<<" ";
+	}
+std::cout<<std::endl;
+	return 0;
+}for(int i=0;i<10;++i)
 {
 int p;
 p=1;
