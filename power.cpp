c9b09e2974756c825de3b8081d4b49f0d0d396f7
rINAtvERESHAGIN <vereshaginrin@gmail.com>
2018-07-11 13:03:24 +0300

    refract

--- power.cpp
+++ power.cpp
@@ -1,8 +1,4 @@
 #include <iostream>
-int main()
-{
-int b=2;
-#include <iostream>
 	int main(){
 int nb=2;
 	for(int ni=0;ni<10;++ni){
@@ -14,14 +10,4 @@ np=1;
 	}
 std::cout<<std::endl;
 	return 0;
-}for(int i=0;i<10;++i)
-{
-int p;
-p=1;
-for(int t=1;t<=i;++t)
-p=p*b;
-std::cout<<p<<" ";
-}
-std::cout<<std::endl;
-return 0;
 }
